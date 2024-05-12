332733398bbdd624517378f19afb706a32ca5ff7
Tom Caraher <info@tomcaraher.com>
2024-05-12 22:05:25 +0100

    Initial commit

---
+++ src/controllers/WearableDeviceAPI.java
@@ -0,0 +1,55 @@
+package controllers;
+
+
+public class WearableDeviceAPI  {
+    //TODO Create a list to store the Wearable Devices
+    //TODO create a File field to story filename
+
+    //TODO create constructor to initialise filename
+
+
+    //todo leave this method in starter
+
+
+
+//TODO - CRUD Methods
+
+
+
+
+    //TODO - Number methods
+
+
+    // TODO Read/list methods
+
+
+    //leave this method in starter
+    //the following is isValidId can be updated
+    //to suit your code
+    /*
+    public boolean isValidId(String id) {
+        for (WearableDevice techDev : whateverYouCalledYourList) {
+            if (techDev.getId().equals(id))
+                return false;
+        }
+            return true;
+        }
+*/
+
+    //TODO get Technology methods
+
+    //TODO - delete methods
+
+
+
+    //TODO - sort methods
+
+    //TODO Top 5 methods
+
+
+
+
+
+    // TODO Persistence methods
+
+}
---
+++ src/utils/ManufacturerNameUtility.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.ArrayList;
+
+public class ManufacturerNameUtility {
+
+    private static ArrayList<String> manufacturerNames = new ArrayList<>(){{
+        add("APPLE");
+        add("SAMSUNG");
+        add("Garmin");
+        add("FitBit");
+        add("Whoop");
+    }};
+
+
+    public static boolean isValidManuName(String name) {
+        //must not be case sensitive
+        for (String manuName:manufacturerNames){
+            if (manuName.equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
